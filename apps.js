3a70ce87128298933cbfe348d2efdeb84b20c354
Stephen Sayage <stephensayage@gmail.com>
2021-01-11 13:52:30 -0500

    updated styling

--- apps.js
+++ apps.js
@@ -1,24 +1,29 @@
-const button = document.getElementById('generate');
-const charDiv = document.getElementById('char-container');
-const gifDiv = document.getElementById('gif');
+// AOS
+AOS.init({
+  duration: 1000,
+});
+//
+const button = document.getElementById("generate");
+const charDiv = document.getElementById("char-container");
+const gifDiv = document.getElementById("gif");
 // -------------------------------------------------------------
 // remove div functions
 function removePeople() {
-  const removeDiv = document.querySelector('#char-container')
+  const removeDiv = document.querySelector("#char-container");
   while (removeDiv.lastChild) {
-    removeDiv.removeChild(removeDiv.lastChild)
+    removeDiv.removeChild(removeDiv.lastChild);
   }
 }
 function removeGif() {
-  const removeDiv = document.querySelector('#gif')
+  const removeDiv = document.querySelector("#gif");
   while (removeDiv.lastChild) {
-    removeDiv.removeChild(removeDiv.lastChild)
+    removeDiv.removeChild(removeDiv.lastChild);
   }
 }
 function removeBar() {
-  const removeDiv = document.querySelector('#bar-container')
+  const removeDiv = document.querySelector("#bar-container");
   while (removeDiv.lastChild) {
-    removeDiv.removeChild(removeDiv.lastChild)
+    removeDiv.removeChild(removeDiv.lastChild);
   }
 }
 // -------------------------------------------------------------
@@ -30,8 +35,8 @@ function getRandomChar(max) {
 // -------------------------------------------------------------
 // load gif and append
 function loadGif() {
-  const portal = document.createElement('img')
-  portal.classList = "portal"
+  const portal = document.createElement("img");
+  portal.classList = "portal";
   portal.src = "https://media.giphy.com/media/3o7aD2d7hy9ktXNDP2/giphy.gif";
   gifDiv.append(portal);
 }
@@ -42,17 +47,17 @@ function loadingBar() {
   if (i == 0) {
     i = 1;
     const bar = document.createElement("div");
-    bar.classList = "load"
-    bar.innerHTML = "Goobling..."
-    document.getElementById('bar-container').append(bar)
-    document.getElementById('bar-container').style.display = "inline";
+    bar.classList = "load";
+    bar.innerHTML = "Wubba Lubba Dub Dub...";
+    document.getElementById("bar-container").append(bar);
+    document.getElementById("bar-container").style.display = "inline";
     let width = 1;
     let id = setInterval(frame, 20);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
-        document.getElementById('bar-container').style.display = "none";
+        document.getElementById("bar-container").style.display = "none";
         removeGif();
       } else {
         width++;
@@ -63,13 +68,16 @@ function loadingBar() {
 }
 // -------------------------------------------------------------
 
-
-button.addEventListener('click', async () => {
-  const url = `https://rickandmortyapi.com/api/character/${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(591)}`
-  const response = await axios.get(`${url}`)
-  const chars = response.data
-
-
+button.addEventListener("click", async () => {
+  const url = `https://rickandmortyapi.com/api/character/${getRandomChar(
+    591
+  )},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(
+    591
+  )},${getRandomChar(591)},${getRandomChar(591)},${getRandomChar(
+    591
+  )},${getRandomChar(591)},${getRandomChar(591)}`;
+  const response = await axios.get(`${url}`);
+  const chars = response.data;
 
   loadGif();
 
@@ -81,29 +89,28 @@ button.addEventListener('click', async () => {
   setTimeout((loadChars) => {
     chars.map((character) => {
       const fixEp = character.episode.map(function (r) {
-        r.replace('https://rickandmortyapi.com/api/', '')
+        r.replace("https://rickandmortyapi.com/api/", "");
         if (character.episode.length > 4) {
           return null;
         } else {
-          return r.replace('https://rickandmortyapi.com/api/', '')
+          return r.replace("https://rickandmortyapi.com/api/", "");
         }
-      })
+      });
 
       charDiv.innerHTML += `
-    <div id="char-id">
-    <img id="gen-img" src=${character.image}>
-    <div id="char-content">
-    <p class="id-num">Id: ${character.id}</p>
-    <p id="gen-name"><span class="span">Name:</span> ${character.name}</p>
-    <p id="gen-species"><span class="span">Species:</span> ${character.species}</p>
-    <p id="gen-origin"><span class="span">Origin:</span> ${character.origin.name}</p>
-    <p id="gen-location"><span class="span">Last Known Location:</span> ${character.location.name}</p>
-    <p id="gen-ep"><span class="span">Episodes:</span> <br> ${fixEp}</p>
-    <img class="logo" src="https://i.imgur.com/gMRuI2T.png">
+    <div class="char-card">
+    <img class="char-img" src=${character.image}>
+    <div class="char-content">
+    <p class="char-id-num">Id: ${character.id}</p>
+    <p class="char-name"><span class="span">Name:</span> ${character.name}</p>
+    <p class="char-species"><span class="span">Species:</span> ${character.species}</p>
+    <p class="char-origin"><span class="span">Origin:</span> ${character.origin.name}</p>
+    <p class="char-location"><span class="span">Last Known Location:</span> ${character.location.name}</p>
+    <p class="char-ep"><span class="span">Episodes:</span> <br> ${fixEp}</p>
+    <img class="char-logo" src="/assets/Logo.png">
     </div>
     </div>
-    `
-    })
+    `;
+    });
   }, 2000);
-})
-
+});
